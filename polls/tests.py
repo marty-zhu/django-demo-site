7feef015403eaefee200e52b05902a31c9eec79f
marty-zhu <marty.zhu@gmail.com>
2022-12-22 08:17:05 -0500

    added detail view publication date tests

--- polls/tests.py
+++ polls/tests.py
@@ -95,3 +95,23 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             [question2, question1]
         )
+
+class QuestionDetailViewTests(TestCase):
+
+    def test_future_question(self):
+        """
+        The detail view of a question with a publication date in the future returns a 404 not found error.
+        """
+        future_question = create_question("Future question.", 30)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a publication date in the past displays the question's text.
+        """
+        past_question = create_question("Past question.", -30)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
