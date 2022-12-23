0bc17ff52a14f24278b90cd4a0c1c533f7e6315d
marty-zhu <marty.zhu@gmail.com>
2022-12-23 09:22:44 -0500

    setup admin template folder and base file

--- demosite/settings.py
+++ demosite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'demosite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # this is a search path for templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
