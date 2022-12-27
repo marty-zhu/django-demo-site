cce517e57193fbb4f102c4a57f3cc0fda5f87104
marty-zhu <marty.zhu@gmail.com>
2022-12-27 14:29:32 -0500

    added Book class to catalog model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Book(models.Model):
+
+    title = models.CharField(max_length=100, help_text='The title of the book')
+    pub_date = models.DateField(help_text='The publication date')
+    authors = models.ManyToManyField(Authors, through='full_name', help_text='The name(s) of the author(s)')
+    summary = models.CharField(max_length=1000, help_text='A summary of the book\'s content')
+    isbn = models.CharField(max_length=20, help_text='The ISBN number of the book', unique=True, primary_key=True)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} by {self.authors}"
