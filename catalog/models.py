fc53d389494582ef821dfe885b234b2c4309bbc7
marty-zhu <marty.zhu@gmail.com>
2022-12-28 09:50:59 -0500

    fixed model Book class author association error

--- catalog/models.py
+++ catalog/models.py
@@ -8,7 +8,7 @@ class Book(models.Model):
 
     title = models.CharField(max_length=200, help_text='The title of the book')
     pub_date = models.DateField(help_text='The publication date')
-    authors = models.ManyToManyField('Authors', through='full_name', help_text='The name(s) of the author(s)', null=True)
+    authors = models.ManyToManyField('Authors', help_text='The name(s) of the author(s)')
     summary = models.CharField(max_length=1000, help_text='A summary of the book\'s content')
     isbn = models.CharField('ISBN', max_length=13, help_text='The ISBN number of the book', unique=True, primary_key=True)
     genre = models.ManyToManyField('Genre', help_text='The genre(s) for this book')
@@ -18,7 +18,7 @@ class Book(models.Model):
         return f"{self.title} by {self.authors}"
 
     def get_absolute_url(self):
-        return reverse('catalog:book-detail', args=[str(self.isbn)])
+        return reverse('catalog:book-detail-view', args=[str(self.isbn)])
 
     class Meta:
         ordering = ['isbn', 'title']
