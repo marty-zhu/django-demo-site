f65111502cb8301a90ce2a12bacb2cc3dab1ed31
marty-zhu <marty.zhu@gmail.com>
2022-12-27 19:22:38 -0500

    updated model Book class properties

--- catalog/models.py
+++ catalog/models.py
@@ -4,12 +4,12 @@ from django.urls import reverse
 # Create your models here.
 class Book(models.Model):
 
-    title = models.CharField(max_length=100, help_text='The title of the book')
+    title = models.CharField(max_length=200, help_text='The title of the book')
     pub_date = models.DateField(help_text='The publication date')
     authors = models.ManyToManyField(Authors, through='full_name', help_text='The name(s) of the author(s)')
     summary = models.CharField(max_length=1000, help_text='A summary of the book\'s content')
-    isbn = models.CharField(max_length=20, help_text='The ISBN number of the book', unique=True, primary_key=True)
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    isbn = models.CharField('ISBN', max_length=13, help_text='The ISBN number of the book', unique=True, primary_key=True)
+    genre = models.ManyToManyField(Genre, help_text='The genre(s) for this book')
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
 
     def __str__(self):
