88d777cb8849e9dbce343aa9756b9f23d8aa9783
marty-zhu <marty.zhu@gmail.com>
2022-12-28 11:35:16 -0500

    Updated catalog Book inline admin display

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,8 +3,19 @@ from django.contrib import admin
 from .models import Book, BookInstance, Author, Genre, Language, User
 
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(BookInstance)
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+class BookAdmin(admin.ModelAdmin):
+    fields = [
+        'title', 'authors', 'pub_date', 'ISBN',
+        'genre', 'language', 'summary'
+    ]
+    inlines = [BookInstanceInline]
+
+admin.site.register(Book, BookAdmin)
+# admin.site.register(BookInstance)
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Language)
