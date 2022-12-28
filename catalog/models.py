e0363a09d813a94a3b31890d3ba4efcb1fce021d
marty-zhu <marty.zhu@gmail.com>
2022-12-27 19:34:39 -0500

    updated model Book class properties

--- catalog/models.py
+++ catalog/models.py
@@ -6,11 +6,11 @@ class Book(models.Model):
 
     title = models.CharField(max_length=200, help_text='The title of the book')
     pub_date = models.DateField(help_text='The publication date')
-    authors = models.ManyToManyField(Authors, through='full_name', help_text='The name(s) of the author(s)')
+    authors = models.ManyToManyField('Authors', through='full_name', help_text='The name(s) of the author(s)', null=True, on_delete=models.SET_NULL)
     summary = models.CharField(max_length=1000, help_text='A summary of the book\'s content')
     isbn = models.CharField('ISBN', max_length=13, help_text='The ISBN number of the book', unique=True, primary_key=True)
-    genre = models.ManyToManyField(Genre, help_text='The genre(s) for this book')
-    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    genre = models.ManyToManyField('Genre', help_text='The genre(s) for this book')
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.title} by {self.authors}"
@@ -30,7 +30,7 @@ class BookInstance(models.Model):
     due_back = models.DateTimeField(blank=True, null=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     imprint = models.CharField(max_length=30)
-    borrower = models.ForeignKey(User, on_delete=models.CASCADE)
+    borrower = models.ForeignKey('User', on_delete=models.CASCADE)
 
     @property
     def status(self):
