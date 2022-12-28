6aad6bc86092da9b5867b376b8de8840695b94b0
marty-zhu <marty.zhu@gmail.com>
2022-12-28 10:35:11 -0500

    Added comments to all model class and methods

--- catalog/models.py
+++ catalog/models.py
@@ -5,6 +5,10 @@ from django.urls import reverse
 
 # Create your models here.
 class Book(models.Model):
+    """
+    Model representation of a book with a unique ISBN,
+    but not specific copies of it in stock.
+    """
 
     title = models.CharField(max_length=200, help_text='The title of the book')
     pub_date = models.DateField(help_text='The publication date')
@@ -15,9 +19,11 @@ class Book(models.Model):
     language = models.ForeignKey('Language', null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
+        """Human-readable string representation for validation."""
         return f"{self.title} by {self.authors}"
 
     def get_absolute_url(self):
+        """Returns the URL to access a detailed record for this book."""
         return reverse('catalog:book-detail-view', kwargs={'pk': self.id})
 
     class Meta:
@@ -26,7 +32,8 @@ class Book(models.Model):
 
 class BookInstance(models.Model):
     """
-    An instance of the book in the catalog. This refers to each individual book that can be lent out under each Book title.
+    An instance of the book in the catalog.
+    This refers to each individual book that can be lent out under each Book title.
     """
     STATUS_MAINTENANCE = 'm'
     STATUS_LOANED = 'o'
@@ -58,10 +65,15 @@ class BookInstance(models.Model):
         ordering = ['due_back']
 
     def __str__(self):
+        """Human-readable string representation for validation."""
         return f"{self.book} with ID {self.copy_id}"
 
 
 class Authors(models.Model):
+    """
+    Model representation of an author of a book.
+    A book can have multiple authors and an author can have multiple books.
+    """
 
     first_name = models.CharField(max_length=20)
     middle_initials = models.CharField(max_length=10, blank=True, null=True)
@@ -71,6 +83,7 @@ class Authors(models.Model):
 
     @property
     def full_name(self):
+        """Returns the full name representation of the author."""
         if self.middle_initials:
             return ' '.join([self.first_name, self.middle_initials, self.last_name])
         else:
@@ -80,29 +93,43 @@ class Authors(models.Model):
         ordering = ['last_name', 'first_name', 'birth_date']
 
     def get_absolute_url(self):
+        """Returns the URL to access a detailed record for this author."""
         return reverse("catalog:author-detail-view", kwargs={"pk": self.id})
 
     def __str__(self):
+        """Human-readable string representation for validation."""
         return self.full_name
 
 
 class Genre(models.Model):
+    """
+    Model representation of the genres of the book.
+    A book can cover multiple genres and a genre covers many books.
+    """
 
     name = models.CharField(max_length=200, help_text='Enter a book genre')
 
     def __str__(self):
+        """Human-readable string representation for validation."""
         return self.name
 
 
 class Language(models.Model):
+    """
+    Model representation of the language the book is written in.
+    """
 
     name = models.CharField(max_length=30)
 
     def __str__(self):
+        """Human-readable string representation for validation."""
         return self.name
 
 
 class User(models.Model):
+    """
+    Model representation of a user who can borrow more than zero books.
+    """
 
     first_name = models.CharField(max_length=20)
     middle_initials = models.CharField(max_length=10, blank=True, null=True)
@@ -110,10 +137,12 @@ class User(models.Model):
 
     @property
     def full_name(self):
+        """Returns the full name representation of the user."""
         if self.middle_initials:
             return ' '.join([self.first_name, self.middle_initials, self.last_name])
         else:
             return ' '.join([self.first_name, self.last_name])
 
     def __str__(self):
+        """Human-readable string representation for validation."""
         return self.full_name
