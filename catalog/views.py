be7c00b48365d5e62f69c002e4feb294ac68cbba
marty-zhu <marty.zhu@gmail.com>
2023-01-01 09:32:28 -0800

    added basic catalog index view

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from .models import *
 
 # Create your views here.
+def index(request):
+    list_of_all_books = Book.objects.all()
+    template = loader.get_template('catalog/index.html')
+    context = {
+        'list_of_all_books': list_of_all_books,
+    }
+    return HttpResponse(template.render(context, request))
