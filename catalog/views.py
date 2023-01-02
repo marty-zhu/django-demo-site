c4c7833d690589e515bd73cc2a9fbdb80d07046f
marty-zhu <marty.zhu@gmail.com>
2023-01-02 09:40:21 -0800

    updated catalog index page to intro

--- catalog/views.py
+++ catalog/views.py
@@ -6,9 +6,15 @@ from .models import *
 
 # Create your views here.
 def index(request):
-    list_of_all_books = Book.objects.all()
+    num_books = Book.objects.all().count()
+    num_authors = Author.objects.all().count()
+    num_genres = Genre.objects.all().count()
+    num_language = Language.objects.all().count()
     template = loader.get_template('catalog/index.html')
     context = {
-        'list_of_all_books': list_of_all_books,
+        'num_books': num_books,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_language': num_language,
     }
     return HttpResponse(template.render(context, request))
