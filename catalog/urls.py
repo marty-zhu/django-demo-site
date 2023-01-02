b63d1e10315afbe962b3277b5eda6c74fc9a6fd2
marty-zhu <marty.zhu@gmail.com>
2023-01-02 10:46:23 -0800

    linked catalog language page

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('books/', views.books, name='books'),
     path('authors/', views.authors, name='authors'),
     path('genres/', views.genres, name='genres'),
+    path('languages/', views.languages, name='languages'),
 ]
