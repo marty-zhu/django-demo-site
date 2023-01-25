80a22fb530661eb30ddb9c1bc2e78aa51d6abbfc
marty-zhu <marty.zhu@gmail.com>
2023-01-25 10:00:15 -0800

    added debug logging to admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,17 +1,32 @@
+import logging
+
+from django import forms
 from django.contrib import admin
+from django.utils.encoding import force_str
 
 from .models import Book, BookInstance, Author, Genre, Language #, User
 
+logging.basicConfig(filename='admin_debug.log', filemode='w', level=logging.DEBUG)
+
 # Register your models here.
+class DebugAdminForm(forms.ModelForm):
+
+    def is_valid(self):
+        logging.debug(force_str(self.errors))
+        return super(DebugAdminForm, self).is_valid()
+
 class BookInstanceInline(admin.TabularInline):
     model = BookInstance
     extra = 0
     fields = ('status', 'loaned_on', 'due_back', 'borrower')
+    form = DebugAdminForm
 
 class BookAdmin(admin.ModelAdmin):
+    form = DebugAdminForm
+    readonly_fields = ('isbn',)
     fields = [
-        'title', 'authors', 'summary',
-        ('isbn', 'genre', 'language')
+        'title', 'authors', 'summary', 'isbn',
+        ('genre', 'language')
     ]
     list_display = ('title', 'display_full_names', 'language')
     inlines = [BookInstanceInline]
@@ -30,8 +45,7 @@ class AuthorAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Book, BookAdmin)
-# admin.site.register(BookInstance)
+admin.site.register(BookInstance)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
 admin.site.register(Language)
-# admin.site.register(User)
