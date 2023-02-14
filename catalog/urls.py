b9e149170a5ee30eef72626913baa916702ec774
marty-zhu <marty.zhu@gmail.com>
2023-02-14 00:28:55 -0500

    added librarian manage user path to catalog URLconf

--- catalog/urls.py
+++ catalog/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('languages/', views.languages, name='languages'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('booksloaned/', views.AllLoanedBooksLibrarianListView.as_view(), name='books-on-loan'),
+    path('librarian/user/<int:pk>', views.manage_user, name='manage-user'),
 ]
