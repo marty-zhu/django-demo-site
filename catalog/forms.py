ecc9b6339ed0f91991281a6adc7ff76de8c4a442
marty-zhu <marty.zhu@gmail.com>
2023-02-17 13:57:50 -0500

    fixed catalog book renew cleaned data bug

--- catalog/forms.py
+++ catalog/forms.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as gtl  # this is for future translation needs
 
 
-EXTENSION_DAYS = [(days, timedelta(days=days)) for days in (3, 7, 14)]
+EXTENSION_DAYS = [(timedelta(days=days), f"{days} days from now") for days in (3, 7, 14)]
 class RenewBookForm(forms.Form):
     extend_days = forms.DurationField(
         required=True,
@@ -15,9 +15,9 @@ class RenewBookForm(forms.Form):
     )
 
     def clean_extend_days(self):
-        data = self.clean_data['extend_days']
+        data = self.cleaned_data['extend_days']
 
-        if data not in [3, 7, 14]:
+        if data not in [tup[0] for tup in EXTENSION_DAYS]:
             raise ValidationError(gtl('Please select the amount of days to extend.'))
 
         return data
