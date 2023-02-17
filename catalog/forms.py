356797f5f5bb78dfb2f78ad954dd410d0cdfc814
marty-zhu <marty.zhu@gmail.com>
2023-02-16 19:22:09 -0500

    fixed catalog book renew form choice format

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django import forms
 
@@ -6,13 +6,12 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as gtl  # this is for future translation needs
 
 
-EXTENSION_DAYS = [3, 7, 14]
-
+EXTENSION_DAYS = [(days, timedelta(days=days)) for days in (3, 7, 14)]
 class RenewBookForm(forms.Form):
     extend_days = forms.DurationField(
         required=True,
         help_text='Select 3, 7, or 14 days for an extension from now.',
-        widget=forms.Select(days=EXTENSION_DAYS),
+        widget=forms.Select(choices=EXTENSION_DAYS),
     )
 
     def clean_extend_days(self):
