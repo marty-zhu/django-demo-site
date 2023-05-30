ff86d71ad9e6527646348827fb2e09c1ed03406f
marty-zhu <marty.zhu@gmail.com>
2023-05-30 01:19:37 -0400

    move catalog add user form validation

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,10 +2,11 @@ from datetime import datetime, timedelta
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as gtl  # this is for future translation needs
+
+from .models import User
 
 
 EXTENSION_DAYS = [(timedelta(days=days), f"{days} days from now") for days in (3, 7, 14)]
@@ -35,11 +36,22 @@ class UserRegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = [
+        fields = (
             'username',
             'first_name',
             'last_name',
             'email',
             'password1',
             'password2',
-        ]
+        )
+    
+    def save(self, commit=True):
+        user = super(UserRegistrationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+        
+        if commit:
+            user.save()
+        
+        return user
