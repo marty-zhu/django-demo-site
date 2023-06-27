106f2947831f103f88d6ee2c7935273e733faf62
marty-zhu <marty.zhu@gmail.com>
2023-06-27 14:54:17 -0400

    removed catalog bookinstance model test TODO

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -267,7 +267,6 @@ class TestBookInstanceModel(TestCase):
         )
 
     def test_book_instance_string(self):
-        # TODO: how to mock the string tracing from Book to Author objects
         expected_string = \
             f"{self.book_instance.book} with ID ending with ..{str(self.book_instance.copy_id)[-6:]}"
         self.assertEqual(str(self.book_instance), expected_string)
