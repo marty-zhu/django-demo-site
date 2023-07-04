c69b1a7e41b62ef41bd7c91e426a72dc3d1b7559
marty-zhu <marty.zhu@gmail.com>
2023-07-04 13:19:40 -0700

    added catalog book renew form text tests

--- catalog/tests/test_forms.py
+++ catalog/tests/test_forms.py
@@ -5,8 +5,14 @@ from catalog.forms import *
 class TestRenewBookForm(TestCase):
     
     def test_form_label(self):
-        pass
+        form = RenewBookForm()
+        self.assertTrue(
+            form.fields['extend_days'].label in ['extend_days', None]
+        )
 
     def test_help_text(self):
-        pass
-
+        form = RenewBookForm()
+        self.assertEqual(
+            form.fields['extend_days'].help_text,
+            'Select 3, 7, or 14 days for an extension from now.'
+        )
