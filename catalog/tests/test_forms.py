cbc26d322323788b5c144407d0c26876cd86435c
marty-zhu <marty.zhu@gmail.com>
2023-07-04 12:55:03 -0700

    added catalog book renew form test template

---
+++ catalog/tests/test_forms.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from catalog.forms import *
+
+
+class TestRenewBookForm(TestCase):
+    
+    def test_form_label(self):
+        pass
+
+    def test_help_text(self):
+        pass
+
