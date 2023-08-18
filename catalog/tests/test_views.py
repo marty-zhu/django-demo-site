ccd915b4473ffc6f9efd5cfc894f9a976343d8e3
marty-zhu <marty.zhu@gmail.com>
2023-08-18 18:40:21 -0400

    created catalog author view test template

---
+++ catalog/tests/test_views.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+from catalog.models import *
+
+
+class TestAuthorListView(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        pass
+    
+    def test_url_accessible_by_locator(self):
+        pass
+
+    def test_url_accessible_by_name(self):
+        pass
+
+    def test_view_template_is_correct(self):
+        pass
+
+    def test_pagination_is_ten(self):
+        pass
+
+    def test_view_list_all_authors(self):
+        pass
